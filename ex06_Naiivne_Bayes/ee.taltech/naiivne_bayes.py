f6db4ea8d85767f97ccd3b564c8413e60bfc5dfc
ARTHUR-ALEKSANDER PÄRNOJA <arthurp200148@gmail.com>
2021-04-22 21:03:22 +0300

    ex06 naiivne bayes.

--- ex06_Naiivne_Bayes/ee.taltech/naiivne_bayes.py
+++ ex06_Naiivne_Bayes/ee.taltech/naiivne_bayes.py
@@ -1,20 +1,22 @@
 import csv
+import math
 from pprint import pprint
 
+# {topic: {word: amount}}
+topic_word_count = {}
 
-def define_topic():
-    # {topic: {word: amount}}
-    topic_word_count = {}
+# {topic: all words in this topic amount}
+topic_all_words = {}
+# {all unique words amount}
+unique_words = set()
 
-    # {topic: all words in this topic amount}
-    topic_all_words = {}
-    # {all unique words amount}
-    unique_words = set()
+# {topic: articles of this topic amount}
+topic_articles = {}
 
+
+def train_topic():
     # all articles counter
     articles = 0
-    # {topic: articles of this topic amount}
-    topic_articles = {}
 
     with open("bbc_train.csv", encoding="utf-8") as f:
         rd = csv.reader(f)
@@ -25,22 +27,55 @@ def define_topic():
             else:
                 topic_articles[topic] = 1
             for word in text.split():
+                word = word.lower()
                 # lowercase tokens longer than 3 chars
-                if len(word) > 3:
-                    unique_words.add(word.lower())
+                if len(word) > 4:
+                    unique_words.add(word)
                     if topic in topic_all_words.keys():
                         topic_all_words[topic] += 1
-                        if word.lower() in topic_word_count[topic].keys():
-                            topic_word_count[topic][word.lower()] += 1
+                        if word in topic_word_count[topic].keys():
+                            topic_word_count[topic][word] += 1
                         else:
-                            topic_word_count[topic][word.lower()] = 1
+                            topic_word_count[topic][word] = 1
                     else:
-                        topic_word_count[topic] = {word.lower(): 1}
+                        topic_word_count[topic] = {word: 1}
                         topic_all_words[topic] = 1
-    pprint(topic_all_words)
-    pprint(len(unique_words))
-    pprint(topic_articles)
-    pprint(articles)
+    return articles
+
+
+def define_topic():
+    correct_topics = 0
+    incorrect_topics = 0
+    articles_amount = train_topic()
+    topic_possibility = {}
+
+    with open("bbc_test.csv", encoding="utf-8") as f:
+        rd = csv.reader(f)
+        for topic, text in rd:
+            for key in topic_articles.keys():
+                topic_possibility[key] = math.log(topic_articles[key] / articles_amount)
+            for word in text.split():
+                word = word.lower()
+                if len(word) > 4:
+                    for key in topic_possibility.keys():
+                        if word in topic_word_count[key].keys():
+                            pwc = (topic_word_count[key][word] + 1) / (topic_all_words[topic] + len(unique_words))
+                        else:
+                            pwc = 1 / (topic_all_words[key] + len(unique_words))
+                        topic_possibility[key] = topic_possibility[key] + math.log(pwc)
+            max_possibility_topic = ""
+            max_possibility = 0
+            for key in topic_possibility.keys():
+                if topic_possibility[key] > max_possibility or max_possibility == 0:
+                    max_possibility = topic_possibility[key]
+                    max_possibility_topic = key
+            if max_possibility_topic == topic:
+                correct_topics += 1
+            else:
+                incorrect_topics += 1
+            topic_possibility.clear()
+    pprint("correct: " + str(correct_topics))
+    pprint("incorrect: " + str(incorrect_topics))
 
 
 if __name__ == '__main__':
